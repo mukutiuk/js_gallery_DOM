95d92e44b1aa666e060b4f7a7d88836135db8437
Roma <roma150269@gmail.com>
2024-06-06 18:41:18 +0300

    make build

---
+++ index.5de88819.js
@@ -0,0 +1,2 @@
+var e=document.querySelectorAll("a"),t=document.querySelector(".gallery__large-img");e.forEach(function(e){e.addEventListener("click",function(r){r.preventDefault(),t.setAttribute("src",e.href)})});
+//# sourceMappingURL=index.5de88819.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,11 @@
+"use strict";
+const link = document.querySelectorAll("a");
+const img = document.querySelector(".gallery__large-img");
+link.forEach((item)=>{
+    item.addEventListener("click", (e)=>{
+        e.preventDefault();
+        img.setAttribute("src", item.href);
+    });
+});
+
+//# sourceMappingURL=index.f75de5e1.js.map
